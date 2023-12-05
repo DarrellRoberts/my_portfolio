08e6de94bd792496c3df3c4b2bc22e8c42093581
DarrellRoberts <mrdarrellroberts@gmail.com>
2023-12-05 20:06:42 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import './App.css'
+import './AppRes.css'
+import Back from "./assets/background.mp4"
+import { DownOutlined } from "@ant-design/icons"
+
+const App: React.FC  = () => {
+
+  return (
+    <>
+    <div>
+      <video 
+      className="" 
+      loop 
+      autoplay="true"
+      playbackRate="0.5"
+      height="500px"
+      muted>
+      <source src={Back} type="video/mp4"/>
+      </video>
+      <div className="name">
+      <h1>Darrell <br/> Roberts</h1>
+      <h2>Fullstack Developer</h2>
+      <DownOutlined 
+      className="V"
+      />
+      </div>
+    </div>
+    <div className="h-screen bg-white">
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: [    
+  "./index.html",
+  "./src/**/*.{js,jsx,ts,tsx}"
+],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "back": 'url("/src/assets/background.mp4")'
+      },
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["responsive", "hover", "focus", "active", "group-hover", "group-focus"],
+    },
+  },
+  plugins: [],
+}
