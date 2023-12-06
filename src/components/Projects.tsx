80d95187b280b87e8643cd6bf66472b213946836
DarrellRoberts <mrdarrellroberts@gmail.com>
2023-12-06 17:14:00 +0100

    added skills & res des

---
+++ src/components/Projects.tsx
@@ -0,0 +1,11 @@
+import "../style/Projects.css"
+
+const Projects = () => {
+    return (
+        <div className="projCon">
+
+        </div>
+    )
+}
+
+export default Projects
---
+++ src/components/SkillsFolder/Skills.tsx
@@ -0,0 +1,54 @@
+// import "../../style/Skills.css"
+
+// const Skills:React.FC = () => {
+//     return (
+//         <div>
+//         <h2 className="skillsTitle">Skills</h2>
+//         <div className="skillsCon">
+
+//         <div className="skillsItem">
+//         <h3>Frontend</h3>
+//         <div className="skillsGrid">
+//         <a href="https://developer.mozilla.org/en-US/docs/Glossary/HTML5" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/html5-colored.svg" width="65" height="65" alt="HTML5" /></a>
+//         <a href="https://www.w3.org/TR/CSS/#css" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/css3-colored.svg" width="65" height="65" alt="CSS3" /></a>
+//         <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/tailwindcss-colored.svg" width="65" height="65" alt="TailwindCSS" /></a>
+//         <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/javascript-colored.svg" width="65" height="65" alt="JavaScript" /></a>
+//         <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/typescript-colored.svg" width="65" height="65" alt="TypeScript" /></a>
+//         <a href="https://reactjs.org/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/react-colored.svg" width="65" height="65" alt="React" /></a>
+//         <a href="https://vitejs.dev/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/vite-colored.svg" width="65" height="65" alt="Vite" /></a>
+//         </div>
+//         </div>
+        
+//         <div className="skillsItem">
+//         <h3>Backend</h3>
+//         <div className="skillsGrid">
+//         <a href="https://nodejs.org/en/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/nodejs-colored.svg" width="65" height="65" alt="NodeJS" /></a>
+//         <a href="https://expressjs.com/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/express-colored.svg" width="65" height="65" alt="Express" /></a>
+//         <a href="https://java.com/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/java-colored.svg" width="65" height="65" alt="Java" /></a>
+//         </div>
+//         </div>
+        
+//         <div className="skillsItem">
+//         <h3>Databases</h3>
+//         <div className="skillsGrid">
+//         <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/mongodb-colored.svg" width="65" height="65" alt="MongoDB" /></a>
+//         <a href="https://www.mysql.com/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/mysql-colored.svg" width="65" height="65" alt="MySQL" /></a>
+//         <a href="https://www.postgresql.org/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/postgresql-colored.svg" width="65" height="65" alt="PostgreSQL" /></a>
+//         <a href="https://render.com/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/render-colored.svg" width="65" height="65" alt="Render" /></a>
+//         </div>
+//         </div>
+        
+//         <div className="skillsItem">
+//         <h3>Tools</h3>
+//         <div className="skillsGrid">
+//         <a href="https://git-scm.com/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/git-colored.svg" width="65" height="65" alt="Git" /></a>
+//         <a href="https://www.docker.com/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/docker-colored.svg" width="65" height="65" alt="Docker" /></a>
+//         <a href="https://www.adobe.com/uk/products/photoshop.html" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/photoshop-colored.svg" width="65" height="65" alt="Photoshop" /></a>
+//         </div>
+//         </div>
+//       </div>
+//     </div>
+//     )
+// }
+
+// export default Skills
