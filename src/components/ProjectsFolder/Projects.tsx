51c2667fb5778b4198d76664a79ad72457d6ed67
DarrellRoberts <mrdarrellroberts@gmail.com>
2023-12-08 00:51:19 +0100

    changed spacing

---
+++ src/components/ProjectsFolder/Projects.tsx
@@ -0,0 +1,15 @@
+import "../../style/Projects.css"
+import "../../style/ProjectsRes.css"
+
+
+const Projects:React.FC = () => {
+    return (
+        <>
+        <div className="projCon">
+            <h3 className="projectTitle">Portfolio</h3>
+        </div>
+        </>
+    )
+}
+
+export default Projects
