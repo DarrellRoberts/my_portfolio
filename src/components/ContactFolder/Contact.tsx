7d536c0a3f87c962654b2e22c3b596ec0b8f3a39
DarrellRoberts <mrdarrellroberts@gmail.com>
2023-12-11 17:21:23 +0100

    added contact con

---
+++ src/components/ContactFolder/Contact.tsx
@@ -0,0 +1,12 @@
+import "../../style/Contact.css"
+
+const Contact:React.FC = () => {
+    return (
+<>
+<div className="h-[500px] bg-white">
+<h2 className="contactTitle">Contact</h2>
+</div>
+</>
+    )
+}
+export default Contact
