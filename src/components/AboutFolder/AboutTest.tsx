67a34a3bb34c19a269113f54bcdb8ef0edda9afb
DarrellRoberts <mrdarrellroberts@gmail.com>
2023-12-18 17:22:25 +0100

    more adjustments made

--- src/components/AboutFolder/AboutTest.tsx
+++ src/components/AboutFolder/AboutTest.tsx
@@ -1,6 +1,5 @@
 import "../../style/About.css"
 import Profilbildbw from "../../assets/Profilbildbw.jpg"
-import SocialLinks from "./SocialLinks"
 import { OverPack } from "rc-scroll-anim";
 import TweenOne from "rc-tween-one";
 import QueueAnim from "rc-queue-anim";
@@ -32,9 +31,9 @@ const AboutTest: React.FC = () => {
         src={Profilbildbw} 
         alt="profile_picture" 
         width="400" 
-        className="ml-20 rounded-lg"
+        className="ppic"
         />) :
-        <div className="ml-20 rounded-lg w-[400px]">
+        <div className="ppic">
           Loading...
           </div>}
         <div className="circle1">
